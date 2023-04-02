18808117796edbacdeb2a83666c24a8fe16ed883
PreetamSing <rathoresinghpreetam@gmail.com>
2023-04-02 23:59:06 +0530

    Update: Brave browser installation.

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -78,3 +78,9 @@ sudo apt install code
 # Install Go lang
 curl -OL https://golang.org/dl/go1.20.2.linux-amd64.tar.gz
 sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.20.2.linux-amd64.tar.gz
+
+# Install Brave browser
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt update
+sudo apt install brave-browser
