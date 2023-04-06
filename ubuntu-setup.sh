4f11382342f027df42f3b6eed3efc0435e6d13af
PreetamSing <rathoresinghpreetam@gmail.com>
2023-04-06 19:08:37 +0530

    Added: FIXME comment for nodejs commands.

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -94,3 +94,5 @@ sudo apt install docker-ce docker-ce-cli containerd.io
 ## Install Docker-compose
 sudo curl -L "https://github.com/docker/compose/releases/download/v2.17.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+
+# FIXME: npm and nodejs commands don't work in fish shell. Probably something with nvm setup.
