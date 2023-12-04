e7bca20463aec73bd040859f84473188d74345e8
PreetamSing <rathoresinghpreetam@gmail.com>
2023-12-04 13:05:27 +0530

    Added neovim binary to PATH env variable.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,2 +1,2 @@
-set PATH $PATH /usr/local/go/bin /usr/sbin ~/.rover/bin
+set PATH $PATH /usr/local/go/bin /usr/sbin ~/.rover/bin ~/_p_pkg_downloads/nvim-linux64/bin
 set --universal nvm_default_version v18.15.0 # this version needs to be updated in "ubuntu-setup.sh" as well.
